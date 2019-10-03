12e4f82a26fa918c6aed4bcafdbd3133e1e30092
Student <student@client1711.(none)>
2019-10-03 15:11:39 +0300

    animation

--- picture1.py
+++ picture1.py
@@ -1,14 +1,23 @@
-# coding: utf-8
-"""
-Использование графического модуля graph.py.
-GR_SIMPLE - простые программы
-  (C) К. Поляков, 2017
-  e-mail: kpolyakov@mail.ru
-  web: http://kpolyakov.spb.ru
-"""
 from graph import *
- 
+
 c = canvas()
+
+def update():
+	for i in obj1:
+		moveObjectBy(i, 5, 0)
+	for j in obj2:	
+		moveObjectBy(j, 5, 0)
+	if xCoord(obj1[0]) >= 1000:
+		for i in obj1:
+		    moveObjectBy(i, -1100, 0)
+	if xCoord(obj2[0]) >= 1000:
+		for i in obj2:
+		    moveObjectBy(i, -1100, 0)
+	
+	
+def keyPressed(event):
+	if event.keycode == VK_ESCAPE:
+		close()
 
 def background(h, d):
     penColor('#000000')
@@ -18,73 +27,86 @@ def background(h, d):
     rectangle(0, 7 / 15 * h, d, 10 / 15 * h)
     brushColor('#eef60c')
     rectangle(0, 10 / 15 * h, d, h)
-    
-def clouds():
-    b_c = 0.7 * 30 #between_clouds
-    r_c = 30 #radius of cloud
-    brushColor('#ffffff')
-    circle(1 / 6 * d, 1 / 6 * h, r_c)
-    circle(1 / 6 * d + b_c, 1 / 6 * h + b_c, r_c)
-    circle(1 / 6 * d + 2 * b_c, 1 / 6 * h, r_c)
-    circle(1 / 6 * d + 3 * b_c, 1 / 6 * h + b_c, r_c)
-    circle(1 / 6 * d + 4 * b_c, 1 / 6 * h, r_c)
-    circle(1 / 6 * d + 5 * b_c, 1 / 6 * h + b_c, r_c)
-    circle(1 / 6 * d + 6 * b_c, 1 / 6 * h, r_c)
 
-def Sun():
-    r_s = 50 # radius of sun
+def clouds(x, y, s):
+    # s - size's cloud
+    b_c = 21 * s #between_clouds
+    r_c = 30 * s #radius of cloud
+    brushColor('#ffffff')
+    circle(x, y, r_c)
+    circle(x + b_c, y + b_c, r_c)
+    circle(x + 2 * b_c, y, r_c)
+    circle(x + 3 * b_c, y + b_c, r_c)
+    circle(x + 4 * b_c, y, r_c)
+
+def Sun(x, y, r_s):
+    # r_s - radius of sun
     penColor('#fff71d')
     brushColor('#fff71d')
-    circle(d - 1 / 7 * d, 1 / 6 * d, r_s)
+    circle(x, y, r_s)
 
 
-def umbrella():
+def umbrella(x, y, s):
     penColor('#e38219')
     brushColor('#e38219')
-    rectangle(1 / 5 * d, 8.5 / 15 * h, 1 / 5 * d + 0.01 * d, 14 / 15 * h)
-    l = d / 10
+    d_stolbik = 0.01 * d * s
+    h_stolbik = 0.43 * h * s
+    d_zont = 3/24 * d * s
+    h_zont = 3/49 * h * s
+    rectangle(x, y, x + d_stolbik, y + h_stolbik)
     penColor('#f45151')
     brushColor('#f45151')
-    polygon([(1 / 5 * d, 8.5 / 15 * h), (1 / 5 * d - l, 9.5 / 15 * h), (1 / 5 * d, 9.5 / 15 * h), (1 / 5 * d, 8.5 / 15 * h)])
-    rectangle(1 / 5 * d, 8.5 / 15 * h, 1 / 5 * d + 0.01 * d, 9.5 / 15 * h)
-    polygon([(1 / 5 * d + 0.01 * d, 8.5 / 15 * h), (1 / 5 * d + 0.01 * d + l, 9.5 / 15 * h), (1 / 5 * d + 0.01 * d, 9.5 / 15 * h), (1 / 5 * d + 0.01 * d, 8.5 / 15 * h)])
+    polygon([(x, y), (x - d_zont, y + h_zont), (x, y + h_zont), (x, y)])
+    rectangle(x, y, x + d_stolbik, y + h_zont)
+    polygon([(x + d_stolbik, y), (x + d_stolbik + d_zont, y + h_zont), (x + d_stolbik, y + h_zont), (x + d_stolbik, y)])
     penColor('#000000')
-    line(1 / 5 * d, 8.5 / 15 * h, 1 / 5 * d - ( 2 / 4 *l), 9.5 / 15 * h)
-    line(1 / 5 * d, 8.5 / 15 * h, 1 / 5 * d - ( 1 / 4 *l), 9.5 / 15 * h)
-    line(1 / 5 * d, 8.5 / 15 * h, 1 / 5 * d - ( 3 / 4 *l), 9.5 / 15 * h)
-    line(1 / 5 * d + 0.01 * d, 8.5 / 15 * h, 1 / 5 * d + 0.01 * d + ( 1 / 4 *l), 9.5 / 15 * h)
-    line(1 / 5 * d + 0.01 * d, 8.5 / 15 * h, 1 / 5 * d + 0.01 * d + ( 2 / 4 *l), 9.5 / 15 * h)
-    line(1 / 5 * d + 0.01 * d, 8.5 / 15 * h, 1 / 5 * d + 0.01 * d + ( 3 / 4 *l), 9.5 / 15 * h)
+    line(x, y, x - 0.1 * d_zont, y + h_zont)
+    line(x, y, x - 0.4 * d_zont, y + h_zont)
+    line(x, y, x - 0.7 * d_zont, y + h_zont)
+    line(x + d_stolbik, y, x + d_stolbik + 0.1 * d_zont, y + h_zont)
+    line(x + d_stolbik, y, x + d_stolbik + 0.4 * d_zont, y + h_zont)
+    line(x + d_stolbik, y, x + d_stolbik + 0.7 * d_zont, y + h_zont)
 
-def ship():
-    d_s = h / 15
-    dl = d - 100
-    c.create_arc(dl * 0.6, h / 2 - d_s, dl * 0.6 + 2 * d_s, h / 2 + d_s, start = 180, extent = 90, fill = '#ba5005')
+def ship(x, y, s):
+    a = []
+    r = 1 / 30 * d * s
+    h_m = 0.005 * d * s
+    a.append(c.create_arc(x - r, y - r, x + r, y + r, start = 180, extent = 90, fill = '#ba5005'))
     penColor('#000000')
     brushColor('#ba5005')
-    rectangle(dl * 0.6 + d_s, h / 2, dl * 0.6 + 5 * d_s, h / 2 + d_s)
-    polygon([(dl * 0.6 + 5 * d_s, h / 2 + d_s), (dl * 0.6 + 7 * d_s, h / 2), (dl * 0.6 + 5 * d_s, h / 2), (dl * 0.6 + 5 * d_s, h / 2 + d_s)])
+    a.append(rectangle(x, y, x + 4 * r, y + r))
+    a.append(polygon([(x + 4 * r, y), (x + 6 * r, y), (x + 4 * r, y + r), (x + 4 * r, y)]))
     brushColor('#000000')
-    rectangle(dl * 0.6 + 2.5 * d_s, h / 2, dl * 0.6 + 2.5 * d_s + 0.01 * d, h / 2 - (h / 5))
+    a.append(rectangle(x + 1.5 * r, y, x + 1.5 * r + h_m, y - 3 * r))
     brushColor('#ded599')
-    polygon([(dl * 0.6 + 2.5 * d_s + 0.01 * d, h / 2 - (h / 5)), (dl * 0.6 + 3.5 * d_s, h / 2 - 0.1 * h), (dl * 0.6 + 5 * d_s, h / 2 - 0.1 * h), (dl * 0.6 + 2.5 * d_s + 0.01 * d, h / 2 - (h / 5))])
-    polygon([(dl * 0.6 + 2.5 * d_s + 0.01 * d, h / 2), (dl * 0.6 + 3.5 * d_s, h / 2 - 0.1 * h), (dl * 0.6 + 5 * d_s, h / 2 - 0.1 * h), (dl * 0.6 + 2.5 * d_s + 0.01 * d, h / 2)])
+    a.append(polygon([(x + 1.5 * r + h_m, y - 3 * r), (x + 2 * r + h_m, y - 1.5 * r), (x + 3 * r + h_m, y - 1.5 * r), (x + 1.5 * r + h_m, y - 3 * r)]))
+    a.append(polygon([(x + 1.5 * r + h_m, y), (x + 2 * r + h_m, y - 1.5 * r), (x + 3 * r + h_m, y - 1.5 * r), (x + 1.5 * r + h_m, y)]))
     brushColor('#000000')
-    circle(dl * 0.6 + 5.4 * d_s, h / 2 + 0.4 * d_s, 10)
+    a.append(circle(x + 13/3 * r, y + 1/3 * r, 1/4 * r))
     brushColor('#ffffff')
-    circle(dl * 0.6 + 5.4 * d_s, h / 2 + 0.4 * d_s, 8)
+    a.append(circle(x + 13/3 * r, y + 1/3 * r, 1/5 * r))
     
-d = 700
-h = 600
+    return a
+d = 1000
+h = 700
 windowSize(d, h)
 canvasSize(d, h)
 background(h, d)
-clouds()
-Sun()
-umbrella()
-ship()
+clouds(0.6 * d, (10 / 75) * h, 0.9)
+clouds( 0.38 * d, (7 / 75) * h, 0.7)
+clouds(0.1 * d, (13 / 75) * h, 1.2)
+Sun(d - 0.123 * d, 0.167 * d, 50)
 
+obj1 = ship(3.8/5 * d, 8/15 * h, 1)
+obj2 = ship(3.5/7 * d, 8.5/15 * h, 0.7)
+for i in obj1:
+	print(type(i))
+for i in obj2:
+	print(type(i))
+umbrella(0.25 * d, 0.55 * h, 1)
+umbrella(0.50 * d, 0.65 * h, 0.7)
+
+onKey(keyPressed)
+onTimer(update, 50)
 run()
-
-
 
