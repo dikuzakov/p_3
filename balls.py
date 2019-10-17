5701041024ff136a7e056d0626a923d32b10f8e4
dikuzakov <danya.kuzakov@gmail.com>
2019-10-17 17:58:01 +0300

    balls1.0

--- balls.py
+++ balls.py
@@ -1,18 +1,14 @@
 from tkinter import *
 from random import randrange as rnd, choice
-import time
-
-cl = ['red','orange','yellow','green','blue']
-
-e = Entry(root, width=20)
-b = Button(root, text = "Очки")
-l = Label(root, bg='black', fg='white', width=20)
-
+import time \
+ \
+ \
 def create_ball():
-    c.delete(ALL)
-    ball = Ball(rnd(100,700), rnd(100,500))
+    ball = Ball(rnd(100, 700), rnd(100, 500))
     ball.draw()
+    c.tag_bind(ball.id, '<Button-1>', ball.delete)
     root.after(1000, create_ball)
+
 
 class Object:
 
@@ -22,28 +18,45 @@ class Object:
     def draw(self):
         pass
 
+
 class Figure(Object):
 
     def __init__(self, color):
         pass
 
+
 class Ball(Figure):
-    
+
     def __init__(self, x_coord, y_coord):
         self.r = rnd(20, 50)
         self.x = x_coord
         self.y = y_coord
 
     def draw(self):
-        c.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = choice(cl))
+        self.id = c.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill=choice(cl))
+
+    def delete(self):
+        c.delete(self)
+
 
 root = Tk()
-c = Canvas(root, width = 800, height = 800, bg = 'white')
+c = Canvas(root, width=800, height=700, bg='white')
 c.pack()
+
+cl = ['red', 'orange', 'yellow', 'green', 'blue']
+
+oval = c.create_oval(30, 10, 130, 80, fill="orange")
+
+
+def oval_func(event):
+    c.delete(oval)
+    c.create_text(80, 50, text="Круг")
+
+
+c.tag_bind(oval, '<Button-1>', oval_func)
 
 create_ball()
 root.mainloop()
-
 """colors = 
 def new_ball():
     canv.delete(ALL)
